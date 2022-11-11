9a4bedf64f78edf48ccf1c427b899d28b69da523
Luan Teixeira <luanlts@outlook.com>
2022-11-11 13:33:25 -0300

    feat: implementado o navegador do react native utilizando pilha

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
 
+// Telas
+import PessoaAddTela from './screens/PessoaAddTela';
+import PessoaListTela from './screens/PessoaListTela';
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/** NavigationContainer responsavel por mapear o estado de navegação do app e fazer os links entre telas
+       * Interage com o SO
+       */}
+      <Stack.Navigator initialRouteName='PessoaAdd'>
+        {/** Stack.Navigator usa a navegacao baseada em pilha */}
+        {/**  Stack.Screen mapeia uma tela com um nome e qual componente deve ser exibibdo */}
+        <Stack.Screen
+          name='PessoaAdd'
+          component={<PessoaAddTela></PessoaAddTela>}
+        />
+        <Stack.Screen
+          name='PessoaList'
+          component={<PessoaListTela></PessoaListTela>}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
